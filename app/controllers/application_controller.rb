8f99bde8581f28373a2128fb390d007064a8f2db
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-21 05:15:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def cart 
       cart = session[:cart] || []
-      if cart.empty?
-       cart << 
+      
   end
 end
